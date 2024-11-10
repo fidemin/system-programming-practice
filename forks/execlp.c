99587724acbf9339149f9206135793df178cda31
Yunhong Min <yunhongmin00@gmail.com>
2024-11-10 20:15:21 +0900

    execlp example

---
+++ forks/execlp.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
+int main(int argc, char *argv[]){
+	printf("running: %d\n", (int) getpid());
+
+	int fd = open("tempfile.txt", O_WRONLY | O_CREAT | O_TRUNC, 0744);
+
+	int pid = fork();
+	if (pid < 0) {
+		fprintf(stderr, "fork failed\n");
+		exit(1);
+	} else if (pid > 0) {
+		// main process
+		wait(NULL);
+	} else {
+		// child process
+		execlp("ls","ls", "-alrth", (char *) NULL);
+        // If execl() fails, this will be printed
+        perror("execl failed");
+        return 1;
+	}
+}
