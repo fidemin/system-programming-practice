dd3980a272e89c98e22e5406cf69a2111f25f746
Yunhong Min <yunhongmin00@gmail.com>
2024-11-10 19:47:53 +0900

    execl example

--- forks/execl.c
+++ forks/execl.c
@@ -6,7 +6,6 @@
 
 int main(int argc, char *argv[]){
 	printf("running: %d\n", (int) getpid());
-	int a = 100;
 
 	int fd = open("tempfile.txt", O_WRONLY | O_CREAT | O_TRUNC, 0744);
 
@@ -16,26 +15,10 @@ int main(int argc, char *argv[]){
 		exit(1);
 	} else if (pid > 0) {
 		// main process
-		const char *text = "parent\n";
-		ssize_t bytes_written = write(fd, text, 7);
-
-		usleep(1000);
-		printf("Good Bye\n");
-		printf("AFTER main: a = %d\n", a);
-		close(fd);
+		wait(NULL);
 	} else {
 		// child process
-		const char *text = "child\n";
-		ssize_t bytes_written = write(fd, text, 6);
-
-		printf("Hello\n");
-
-		printf("BEFORE child: a = %d\n", a);
-		a = 101;
-		printf("AFTER child: a = %d\n", a);
-
-
-		execl("/bin/ls", "ls", "-l", (char *) NULL);
+		execl("/bin/ls", "ls", "-alrth", (char *) NULL);
         // If execl() fails, this will be printed
         perror("execl failed");
         return 1;
